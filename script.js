7634bc83d55c43e544482f925845d29de6390882
techymathu1-debug <techymathu1@gmail.com>
2026-05-06 13:47:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function openPage() {
+  document.getElementById("landing").style.display = "none";
+  document.getElementById("content").classList.remove("hidden");
+}
+const blocks = document.querySelectorAll(".block");
+
+function revealOnScroll() {
+  const triggerBottom = window.innerHeight * 0.8;
+
+  blocks.forEach(block => {
+    const boxTop = block.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      block.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+// run once on load
+revealOnScroll();
